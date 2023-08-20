7fba1e8f0db3e5704e9e5d4f3d01a78ad20ea40a
Gabriel Schumann <108986088+schumann7@users.noreply.github.com>
2023-08-20 20:23:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleSearchInput() {
+    var searchInput = document.querySelector(".search-input");
+    searchInput.style.display = searchInput.style.display === "block" ? "none" : "block";
+  }
