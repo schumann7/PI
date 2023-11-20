4a346ed666f38761e8b0a43281df61b4470706f0
Gabriel Schumann <108986088+schumann7@users.noreply.github.com>
2023-11-20 20:14:29 -0300

    Commit pra apresentação final

--- script.js
+++ script.js
@@ -20,17 +20,19 @@ function Contraste(){
         document.getElementById('saibamais').style.border = 'solid'
         document.getElementById('botaoartigos').style.backgroundColor = 'black'
         document.getElementById('botaoartigos').style.border = 'solid'
+        document.getElementById('botaovoltarimagens').style.backgroundColor = 'black'
+        document.getElementById('botatopularimagens').style.backgroundColor = 'black'
     }
     else{
         contraste=0
-        document.getElementById('header').style.backgroundColor = 'white'
-        document.getElementById('body').style.backgroundColor = 'white'
+        document.getElementById('header').style.backgroundColor = '#F5F2E6'
+        document.getElementById('body').style.backgroundColor = '#F5F2E6'
         document.getElementById('titulo').style.color = 'black'
         document.getElementById('link').style.color = 'black'
         document.getElementById('link1').style.color = 'black'
         document.getElementById('link2').style.color = 'black'
         document.getElementById('body').style.color = 'black'
-        document.getElementById('footer').style.backgroundColor = '#577272'
+        document.getElementById('footer').style.backgroundColor = '#216b6b'
         document.getElementById('pergunta').style.color = '#409d9d'
         document.getElementById('dicas').style.color = '#409d9d'
         document.getElementById('leidecotas1').style.color = '#409d9d'
@@ -38,6 +40,8 @@ function Contraste(){
         document.getElementById('saibamais').style.border = 'none'
         document.getElementById('botaoartigos').style.backgroundColor = '#409d9d'
         document.getElementById('botaoartigos').style.border = 'none'
+        document.getElementById('botaovoltarimagens').style.backgroundColor = '#409d9d'
+        document.getElementById('botatopularimagens').style.backgroundColor = '#409d9d'
     }
 }
 let tamanhotitulo = 32
@@ -45,8 +49,8 @@ let tamanholink = 18
 let tamanholeidecotas = 23
 let tamanholorem = 16
 let tamanhosaibamais = 19
-let tamanhopli = 12
-let tamanholeidecotas1 = 28
+let tamanhopli = 15
+let tamanholi = 19
 function Aumentar(){
     tamanhotitulo+=1
     tamanholink+=1
@@ -54,7 +58,7 @@ function Aumentar(){
     tamanholorem+=1
     tamanhosaibamais+=1
     tamanhopli+=1
-    tamanholeidecotas1+=1
+    tamanholi+=1
     document.getElementById('titulo').style.fontSize = tamanhotitulo + "pt";
     document.getElementById('link').style.fontSize = tamanholink + "pt";
     document.getElementById('link1').style.fontSize = tamanholink + "pt";
@@ -64,14 +68,14 @@ function Aumentar(){
     document.getElementById('lorem').style.fontSize = tamanholorem + "pt";
     document.getElementById('saibamais').style.fontSize = tamanhosaibamais + "pt";
     document.getElementById('dicas').style.fontSize = tamanhotitulo + "pt";
-    document.getElementById('li').style.fontSize = tamanholorem + "pt";
-    document.getElementById('li1').style.fontSize = tamanholorem + "pt";
-    document.getElementById('li2').style.fontSize = tamanholorem + "pt";
-    document.getElementById('li3').style.fontSize = tamanholorem + "pt";
-    document.getElementById('li4').style.fontSize = tamanholorem + "pt";
-    document.getElementById('li5').style.fontSize = tamanholorem + "pt";
-    document.getElementById('li6').style.fontSize = tamanholorem + "pt";
-    document.getElementById('li7').style.fontSize = tamanholorem + "pt";
+    document.getElementById('li').style.fontSize = tamanholi + "pt";
+    document.getElementById('li1').style.fontSize = tamanholi + "pt";
+    document.getElementById('li2').style.fontSize = tamanholi + "pt";
+    document.getElementById('li3').style.fontSize = tamanholi + "pt";
+    document.getElementById('li4').style.fontSize = tamanholi + "pt";
+    document.getElementById('li5').style.fontSize = tamanholi + "pt";
+    document.getElementById('li6').style.fontSize = tamanholi + "pt";
+    document.getElementById('li7').style.fontSize = tamanholi + "pt";
     document.getElementById('pli').style.fontSize = tamanhopli + "pt";
     document.getElementById('pli1').style.fontSize = tamanhopli + "pt";
     document.getElementById('pli2').style.fontSize = tamanhopli + "pt";
@@ -80,7 +84,7 @@ function Aumentar(){
     document.getElementById('pli5').style.fontSize = tamanhopli + "pt";
     document.getElementById('pli6').style.fontSize = tamanhopli + "pt";
     document.getElementById('pli7').style.fontSize = tamanhopli + "pt";
-    document.getElementById('leidecotas1').style.fontSize = tamanholeidecotas1 + "pt";
+    document.getElementById('leidecotas1').style.fontSize = tamanhotitulo + "pt";
     document.getElementById('txtlei').style.fontSize = tamanholorem + "pt";
     document.getElementById('botaoartigos').style.fontSize = tamanhosaibamais + "pt";
 }
@@ -91,7 +95,7 @@ function Diminuir(){
     tamanholorem-=1
     tamanhosaibamais-=1
     tamanhopli-=1
-    tamanholeidecotas1-=1
+    tamanholi-=1
     document.getElementById('titulo').style.fontSize = tamanhotitulo + "pt";
     document.getElementById('link').style.fontSize = tamanholink + "pt";
     document.getElementById('link1').style.fontSize = tamanholink + "pt";
@@ -101,14 +105,14 @@ function Diminuir(){
     document.getElementById('lorem').style.fontSize = tamanholorem + "pt";
     document.getElementById('saibamais').style.fontSize = tamanhosaibamais + "pt";
     document.getElementById('dicas').style.fontSize = tamanhotitulo + "pt";
-    document.getElementById('li').style.fontSize = tamanholorem + "pt";
-    document.getElementById('li1').style.fontSize = tamanholorem + "pt";
-    document.getElementById('li2').style.fontSize = tamanholorem + "pt";
-    document.getElementById('li3').style.fontSize = tamanholorem + "pt";
-    document.getElementById('li4').style.fontSize = tamanholorem + "pt";
-    document.getElementById('li5').style.fontSize = tamanholorem + "pt";
-    document.getElementById('li6').style.fontSize = tamanholorem + "pt";
-    document.getElementById('li7').style.fontSize = tamanholorem + "pt";
+    document.getElementById('li').style.fontSize = tamanholi + "pt";
+    document.getElementById('li1').style.fontSize = tamanholi + "pt";
+    document.getElementById('li2').style.fontSize = tamanholi + "pt";
+    document.getElementById('li3').style.fontSize = tamanholi + "pt";
+    document.getElementById('li4').style.fontSize = tamanholi + "pt";
+    document.getElementById('li5').style.fontSize = tamanholi + "pt";
+    document.getElementById('li6').style.fontSize = tamanholi + "pt";
+    document.getElementById('li7').style.fontSize = tamanholi + "pt";
     document.getElementById('pli').style.fontSize = tamanhopli + "pt";
     document.getElementById('pli1').style.fontSize = tamanhopli + "pt";
     document.getElementById('pli2').style.fontSize = tamanhopli + "pt";
@@ -117,7 +121,25 @@ function Diminuir(){
     document.getElementById('pli5').style.fontSize = tamanhopli + "pt";
     document.getElementById('pli6').style.fontSize = tamanhopli + "pt";
     document.getElementById('pli7').style.fontSize = tamanhopli + "pt";
-    document.getElementById('leidecotas1').style.fontSize = tamanholeidecotas1 + "pt";
+    document.getElementById('leidecotas1').style.fontSize = tamanhotitulo + "pt";
     document.getElementById('txtlei').style.fontSize = tamanholorem + "pt";
     document.getElementById('botaoartigos').style.fontSize = tamanhosaibamais + "pt";
 }
+
+var imagens = ["imagem1.jpg", "imagem2.jpg", "imagem3.png", "imagem4.jpg", "imagem5.jpg", "imagem6.jpg", "imagem7.jpg", "imagem8.jpg"];
+        var indiceImagemAtual = 0;
+      
+        function mudarImagem() {
+          for (var i = 1; i <= 4; i++) {
+            document.getElementById("imagem" + i).src = imagens[(indiceImagemAtual + i - 1) % imagens.length];
+          }
+          indiceImagemAtual = (indiceImagemAtual + 1) % imagens.length;
+        }
+        
+        function voltarImagem() {
+          for (var i = 1; i <= 4; i++) {
+            document.getElementById("imagem" + i).src = imagens[(indiceImagemAtual - i + imagens.length) % imagens.length];
+          }            
+          indiceImagemAtual = (indiceImagemAtual - 1 + imagens.length) % imagens.length;
+        }
+            
